db926477477837412139a0366b0710a0f9899632
MASKY-33 <masky.somebody33@gmail.com>
2026-06-30 21:02:09 +0200

    Update and rename secured-digital-bank-vault.py to auth.py

--- auth.py
+++ auth.py
@@ -42,36 +42,3 @@ def withdrawal_money(account_id, amount):
             
     except ValueError:
         return "Error: Login-ID and/or withdrawing must be numbers!"
-
-
-while True:
-    print("\n--- Welcome at you Digitak Bank-Vault! ---")
-    print("1. View balance")
-    print("2; Withdrawal money")
-    print("3. Close.")
-
-
-    choice = input("Choose option (1-3): ")
-
-
-    if choice == "3" or choice.lower() == "stop":
-        print("Thankyou, Bye!")
-        break
-
-
-    elif choice == "1":
-        id_request = input("Give in your login-ID: ")
-
-        id_check = check_balance(id_request)
-        print(id_check)
-
-
-    elif choice == "2":
-        id_request = input("Give in your login-ID: ")
-        amount_request = input("How much money do you want to withdraw?: ")
-
-        balance_check = withdrawal_money(id_request, amount_request)
-        print(balance_check)
-
-    else:
-        print("Invalid choice, try again!")
